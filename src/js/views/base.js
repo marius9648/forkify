12afb072d40b6cc81c33c4d40564b52a6b973520
Marius <marius9648@yahoo.com>
2020-01-11 17:05:40 +0200

    add pagination buttons

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -2,7 +2,8 @@ export const elements = {
     searchInput: document.querySelector('.search__field'),
     searchForm: document.querySelector('.search'),
     searchResultList: document.querySelector('.results__list'),
-    searchResult: document.querySelector('.results')
+    searchResult: document.querySelector('.results'),
+    searchResPages: document.querySelector('.results__pages')
 };
 
 export const renderLoader = parent => {
